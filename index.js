f4cc193f7cd02411f2aa6a173f8e69122682dfb8
Nma Shawkat <nmashawket@gmail.com>
2022-12-04 16:55:05 +0300

    donnnee

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// const tutorials = [
+//   'what does the this keyword mean?',
+//   'What is the Constructor OO pattern?',
+//   'implementing Blockchain Web API',
+//   'The Test Driven Development Workflow',
+//   'What is NaN and how Can we Check for it',
+//   'What is the difference between stopPropagation and preventDefault?',
+//   'Immutable State and Pure Functions',
+//   'what is the difference between == and ===?',
+//   'what is the difference between event capturing and bubbling?',
+//   'what is JSONP?'
+// ];
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +28,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
+//
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(eachWord => eachWord.split(' ').map(myTutorial => myTutorial.charAt(0).toUpperCase() + myTutorial.slice(1)).join(' '))
 }
